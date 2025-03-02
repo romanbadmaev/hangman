13f253860378ddb005171e39a9ea4d29e9f97366
roma <captainburyatia@gmail.com>
2025-03-03 05:52:12 +0800

    02.03.25

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ from words import guess_word
 from ascii import draw_hangman
 
 game_over = False # условие выхода из игры
+victory = False # победа в игре
+fiasco = False # поражение в игре
+word, masked_word = guess_word()
+mistakes_number = 0 # количество ошибок
+wrong_letters = set() # множество неправильных ответов
+right_letters = set() # множество правильных ответов
+
 cyrillic = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-" # буквы русского алфавита
 
-def play_game(*, word: str):
+def play_game(*,
+    word:str="Виселица",
+    masked_word:str="В______а",
+    mistakes_number:int=0,
+    wrong_letters:set=set(),
+    right_letters:set=set(),
+    victory:bool=False,
+    fiasco:bool=False):
     """
-    функция которая ведет игру
-    :param word: загаданное слово
+    загадывание слова
+    :param word:
+    :param masked_word:
+    :param mistakes_number:
+    :param wrong_letters:
+    :param right_letters:
+    :param victory:
+    :param fiasco:
     :return:
     """
-    global mistakes_number
-    global wrong_letters
-    global right_letters
-    global masked_word
-    global victory
-    global fiasco
-
     draw_hangman(mistakes=mistakes_number)
     print(f"Ошибки: {mistakes_number} из 6. Неверные ответы: {wrong_letters if wrong_letters else ""}")
     letter = input(f"Слово: {masked_word}. Назовите букву: ") # ввод буквы - ответа:
@@ -36,7 +49,13 @@ def play_game(*, word: str):
             mistakes_number += 1
         if mistakes_number > 5:
             fiasco = True
-
+    return (
+        masked_word,
+        mistakes_number,
+        wrong_letters,
+        right_letters,
+        victory,
+        fiasco)
 
 draw_hangman(mistakes=0) # заставка
 # запуск и перезапуск игры
@@ -51,7 +70,19 @@ while not game_over:
         wrong_letters = set()
         right_letters = set()
         while not victory and not fiasco:
-            play_game(word=word)
+            masked_word,
+            mistakes_number,
+            wrong_letters,
+            right_letters,
+            victory,
+            fiasco = play_game(
+                word=word,
+                masked_word=masked_word,
+                mistakes_number=mistakes_number,
+                wrong_letters=wrong_letters,
+                right_letters=right_letters,
+                victory=victory,
+                fiasco=fiasco)
         else:
             draw_hangman(mistakes=mistakes_number)
             print(f"Ошибки: {mistakes_number} из 6. Неверные ответы: {
